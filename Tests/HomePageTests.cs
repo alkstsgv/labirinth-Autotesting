9ce113a76cd6e85869d860b582f0eedbdc5668b6
tsygankov <tsygankov@socrat.marketing>
2025-10-26 13:23:31 +1000

    тс1 сделан, но не полностью по POM

--- Tests/HomePageTests.cs
+++ Tests/HomePageTests.cs
@@ -27,7 +27,7 @@ public class HomePageTester : BaseTest
 	{
 		Console.WriteLine("Тест-кейс 1. Добавление книги в «Отложено» по кнопке «Сердце» в блоке «Что почитать: выбор редакции»");
 
-		// === Подготовка === //
+		// === Подготовка теста === //
 		Console.WriteLine("Выполнение предусловий");
 		await GotoAsync("/");
 		await _homePage.AcceptModalWithCookies();
@@ -46,13 +46,13 @@ public class HomePageTester : BaseTest
 		var popupAfterAddToFavList = Page.Locator("#minwidth .js-good-added");
 		var popupBookTitle = popupAfterAddToFavList.Locator(".b-basket-popinfo-e-text-m-add a[href]");
 		string? popupBookTitleName = await popupBookTitle.GetAttributeAsync("href");
-		var heartInNavbar = Page.Locator("#minwidth .top-link-main_putorder");
+		var heartInNavbar = Page.Locator("#minwidth .top-link-main_putorder span.basket-in-dreambox-a");
 		string? heartInNavbarNumbers = await heartInNavbar.InnerTextAsync();
-		Console.WriteLine(heartInNavbarNumbers);
 
 		await _playwright.Expect(element).ToBeAttachedAsync();
 		await _playwright.Expect(element).ToBeVisibleAsync();
 		await _playwright.Expect(element).ToHaveCSSAsync("color", "rgb(173, 10, 5)");
+
 		await _playwright.Expect(popupAfterAddToFavList).ToBeAttachedAsync();
 		await _playwright.Expect(popupAfterAddToFavList).ToBeVisibleAsync();
 		await _playwright.Expect(popupAfterAddToFavList).ToBeInViewportAsync();
@@ -65,3 +65,4 @@ public class HomePageTester : BaseTest
 	}
 
 }
+// #minwidth > div.top-header > div.b-header-outer > div.b-header > div.b-header__top-part.js-header-top-part > div.b-header-b-personal.col-xs-6.col-sm-6.col-md-5.col-md-push-5.col-lg-push-6.col-lg-4.col-xxl-push-7.col-xxl-3.js-toggle-menu-block > div > ul > li:nth-child(5) > a > span.b-header-b-personal-e-icon-wrapper.\<\!--b-header-b-personal-e-icon-wrapper-m-putorder--\> > span.b-header-b-personal-e-wrapper-m-closed > span.b-header-b-personal-e-icon-count-m-putorder.basket-in-dreambox-a
